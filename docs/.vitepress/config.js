60a3c7001b1d2b5dcca1b15f4046d7f12603bd65
Jacen <jacen@local.dev>
2026-07-15 16:14:41 +0800

    fix: add cache busting to favicon URLs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -19,9 +19,9 @@ export default defineConfig({
     ['meta', { charset: 'utf-8' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1' }],
     ['meta', { name: 'author', content: 'Jacen Hu' }],
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
-    ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    ['link', { rel: 'apple-touch-icon', href: '/favicon.svg' }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg?v=2' }],
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=2' }],
+    ['link', { rel: 'apple-touch-icon', href: '/favicon.svg?v=2' }],
     ['link', { rel: 'dns-prefetch', href: 'https://github.com' }],
     ['link', { rel: 'preconnect', href: 'https://github.com' }]
   ],
